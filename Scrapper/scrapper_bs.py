faf19a46bea6fdb24866a4459e467360f67752ba
SandyDRawat <sr9644774485@gmail.com>
2024-10-22 21:42:07 +0530

    Remove the next url function from scrapper_bs

--- Scrapper/scrapper_bs.py
+++ Scrapper/scrapper_bs.py
@@ -6,12 +6,12 @@ def scrapper_bs(url):
     if req.status_code == 200:
         req.encoding = 'utf-8'
         soup = BeautifulSoup(req.text, "html.parser")
-        next_chapter_link = soup.find('a', id='next_chap')['href']
+        
         paragraphs = soup.find_all('p')
         chapter_content = ''
         for p in paragraphs:
             chapter_content += str(p)  # Convert to string to preserve HTML tags
-        return chapter_content, next_chapter_link
+        return chapter_content
     else:
         print("Failed to retrieve content from the URL.")
-        return None, None
+        return None
