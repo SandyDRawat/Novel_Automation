3282c48dcc35a7f03be2939e4e2dc352a5fa5537
SandyDRawat <sr9644774485@gmail.com>
2024-12-05 18:39:17 +0530

    Add chinese web scrapping capabilities

--- Web_crawler/link_retrieve.py
+++ Web_crawler/link_retrieve.py
@@ -6,7 +6,10 @@ def link_retriever(url):
     if req.status_code == 200:
         req.encoding = 'utf-8'
         soup = BeautifulSoup(req.text, "html.parser")
-        next_chapter_link = soup.find('a', id='next_chap')['href']  
+        try:
+            next_chapter_link = soup.find('a', id='next_chap')['href']
+        except:
+            next_chapter_link = "https://www.ptwxw.com" + soup.find('a', id='next_url')['href']
         return next_chapter_link
     else:
         print("Failed to retrieve next ling from the URL.")
--- Web_crawler/scrapper_bs.py
+++ Web_crawler/scrapper_bs.py
@@ -6,8 +6,17 @@ def scrapper_bs(url):
     if req.status_code == 200:
         req.encoding = 'utf-8'
         soup = BeautifulSoup(req.text, "html.parser")
-        paragraphs = soup.find_all('p')
-        chapter_title = soup.find('span', class_="chr-text").text
+        
+        try:
+            content_element = soup.find('div', class_="chr-c")
+            chapter_title = soup.find('span', class_="chr-text").text
+            
+        except:
+            content_element = soup.find('div', class_="read-content")
+            chapter_title = soup.find('h1').text
+        
+        paragraphs = content_element.find_all('p')
+        
         chapter_content = ''
         for p in paragraphs:
             chapter_content += str(p)  # Convert to string to preserve HTML tags
@@ -23,5 +32,5 @@ def compiled_scrapper(urls):
         chapter_content, chapter_title = scrapper_bs(url)
         if chapter_content:
             chapter_contents.append(chapter_content)
-            chapter_titles.append(chapter_title.split('                                ')[1])
+            chapter_titles.append(chapter_title)
     return chapter_contents, chapter_titles
